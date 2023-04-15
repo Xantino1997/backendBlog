4bac608ee392ac543944a29aadad8a27af84e470
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-04-14 22:56:47 -0300

    add vercel and railway

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertj';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://blog3-eta.vercel.app' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
