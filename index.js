e220ac64b6ec3cd89dca33c60f143801d137bfae
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-06 10:48:03 -0300

    gat of the server

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
@@ -42,9 +44,9 @@ app.options('/post', (req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', 'https://blog3-eta.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.status(200).send();
+  next();
+
 });
 
 
