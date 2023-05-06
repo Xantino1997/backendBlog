bf54171ebf8478e227129e4f3c5b0dc19f4e50a6
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-06 07:46:17 -0300

    credentials true

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
