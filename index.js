5dc91c608cb5518bee0268f3dcf33361f08bf43c
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-11 00:15:03 -0300

    delete add api/

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ mongoose.connect(uri, {
 
 
 
-app.post('/api/register', uploadMiddleware.single('profilePicture'), async (req, res) => {
+app.post('/register', uploadMiddleware.single('profilePicture'), async (req, res) => {
   const { username, password } = req.body;
   const { originalname, path } = req.file;
   const parts = originalname.split('.');
@@ -98,12 +98,12 @@ app.post('/api/register', uploadMiddleware.single('profilePicture'), async (req,
 
 
 
-app.post('/api/login', async (req, res) => {
+app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
-    // login
+    // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
       res.cookie('token', token).json({
@@ -122,7 +122,7 @@ app.post('/api/login', async (req, res) => {
 
 
 
-app.get('/api/profile', (req, res) => {
+app.get('/profile', (req, res) => {
   const { token } = req.cookies;
   if (!token) {
     return res.status(401).json({ message: 'No se proporcionó un token' });
@@ -139,7 +139,7 @@ app.get('/api/profile', (req, res) => {
 
 
 
-app.post('/api/logout', (req, res) => {
+app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok');
 });
 
@@ -166,7 +166,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 
 });
 
-app.put('/api/post/:id',uploadMiddleware.single('file'), async (req,res) => {
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
   let newPath = null;
   if (req.file) {
     const {originalname,path} = req.file;
@@ -203,7 +203,7 @@ app.put('/api/post/:id',uploadMiddleware.single('file'), async (req,res) => {
 
 
 
-app.get('/api/post', async (req, res) => {
+app.get('/post', async (req, res) => {
   res.json(
     await Post.find()
       .populate('author', ['username'])
@@ -212,7 +212,7 @@ app.get('/api/post', async (req, res) => {
   );
 });
 
-app.get('/api/post/:id', async (req, res) => {
+app.get('/post/:id', async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
