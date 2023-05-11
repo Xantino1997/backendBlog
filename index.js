8b68ae1b8ef1db6ba2d0c5be913bf29807d0cb57
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-11 00:06:39 -0300

    add api/

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ mongoose.connect(uri, {
 
 
 
-app.post('/register', uploadMiddleware.single('profilePicture'), async (req, res) => {
+app.post('/api/register', uploadMiddleware.single('profilePicture'), async (req, res) => {
   const { username, password } = req.body;
   const { originalname, path } = req.file;
   const parts = originalname.split('.');
@@ -98,7 +98,7 @@ app.post('/register', uploadMiddleware.single('profilePicture'), async (req, res
 
 
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
@@ -122,7 +122,7 @@ app.post('/login', async (req, res) => {
 
 
 
-app.get('/profile', (req, res) => {
+app.get('/api/profile', (req, res) => {
   const { token } = req.cookies;
   if (!token) {
     return res.status(401).json({ message: 'No se proporcionó un token' });
@@ -139,7 +139,7 @@ app.get('/profile', (req, res) => {
 
 
 
-app.post('/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
   res.cookie('token', '').json('ok');
 });
 
@@ -166,6 +166,36 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 
 });
 
+app.put('/api/post/:id',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
+});
 
 
 
@@ -173,7 +203,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 
 
 
-app.get('/post', async (req, res) => {
+app.get('/api/post', async (req, res) => {
   res.json(
     await Post.find()
       .populate('author', ['username'])
@@ -182,7 +212,7 @@ app.get('/post', async (req, res) => {
   );
 });
 
-app.get('/post/:id', async (req, res) => {
+app.get('/api/post/:id', async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
