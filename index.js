479341f71f8c77dc133e3276883c2d8f0bf7aa50
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-14 23:09:24 -0300

    delet *

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 
-// sin paquete cors
+// sin paquete cors 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://sentidos-blog.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
