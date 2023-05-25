e23a226256684fa96a463ee144d6c3eb4bdb4c19
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-05-24 21:44:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require("mongoose");
-// const User = require('./models/User');
-// const Post = require('./models/Post');
-// const Suscriptor = require('./models/Suscribe');
-// const bcrypt = require('bcryptjs');
-// const app = express();
-// const jwt = require('jsonwebtoken');
-// const cookieParser = require('cookie-parser');
-// const multer = require('multer');
-// const nodemailer = require('nodemailer');
-// const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const Post = require('./models/Post');
+const Suscriptor = require('./models/Suscribe');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
+const path = require('path');
 
 
-// const uploadMiddleware = multer({
-//   dest: 'uploads/',
-//   limits: {
-//     fileSize: 10 * 1024 * 1024 // 10 megabytes
-//   }
-// });
+const uploadMiddleware = multer({
+  dest: 'uploads/',
+  limits: {
+    fileSize: 10 * 1024 * 1024 // 10 megabytes
+  }
+});
 
-// const fs = require('fs');
-// const dotenv = require('dotenv').config();
+const fs = require('fs');
+const dotenv = require('dotenv').config();
 
-// const port = process.env.PORT || 4000;
-// const uri = process.env.REACT_APP_URI
+const port = process.env.PORT || 4000;
+const uri = process.env.REACT_APP_URI
 
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// // Usar body-parser para procesar los datos en el cuerpo de las solicitudes entrantes
-// const jsonParser = bodyParser.json({ limit: '50mb' });
-// const urlencodedParser = bodyParser.urlencoded({ limit: '50mb', extended: true });
+// Usar body-parser para procesar los datos en el cuerpo de las solicitudes entrantes
+const jsonParser = bodyParser.json({ limit: '50mb' });
+const urlencodedParser = bodyParser.urlencoded({ limit: '50mb', extended: true });
 
-// // utilizar los analizadores de cuerpo en la aplicación
-// app.use(jsonParser);
-// app.use(urlencodedParser);
+// utilizar los analizadores de cuerpo en la aplicación
+app.use(jsonParser);
+app.use(urlencodedParser);
 
-// const salt = bcrypt.genSaltSync(10);
-// const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-// // app.use(cors({credentials:true,origin:'https://sentidos-blog.vercel.app'}));
-// // sin paquete cors
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'https://sentidos.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Credentials', 'true');
+// app.use(cors({credentials:true,origin:'https://sentidos-blog.vercel.app'}));
+// sin paquete cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://sentidos.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
-//   next();
-// });
-
-
-
-
-// // con el  paquete cors
-
-// // app.use(cors({
-// //   origin: 'https://sentidos-blog.vercel.app',
-// //   methods: ['POST', 'PUT', 'GET'],
-// //   allowedHeaders: ['Content-Type', 'Authorization']
-// // }));
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use('/uploads', express.static(__dirname + '/uploads'));
+  next();
+});
 
 
 
 
+// con el  paquete cors
+
+// app.use(cors({
+//   origin: 'https://sentidos-blog.vercel.app',
+//   methods: ['POST', 'PUT', 'GET'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
 
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,    // usa el nuevo parser de URL
-//   useUnifiedTopology: true, // utiliza la nueva topología unificada
 
-// }).then(() => {
-//   console.log('Conexión exitosa a la base Mongo database');
-// }).catch((error) => {
-//   console.log('Error al conectar a la base de datos:', error);
-// });
 
-// // app.post('/test', (req, res, next) => {
-// //   res.json({ message: 'Hello world' });
-// // });
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,    // usa el nuevo parser de URL
+  useUnifiedTopology: true, // utiliza la nueva topología unificada
+
+}).then(() => {
+  console.log('Conexión exitosa a la base Mongo database');
+}).catch((error) => {
+  console.log('Error al conectar a la base de datos:', error);
+});
 
 
 
@@ -192,225 +188,6 @@
 //   }
 // });
 
-
-
-
-
-// app.post('/login', async (req, res) => {
-
-//   const { username, password } = req.body;
-//   const userDoc = await User.findOne({ username });
-//   const passOk = bcrypt.compareSync(password, userDoc.password);
-//   if (passOk) {
-//     // logged in
-//     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-//       if (err) throw err;
-//       res.cookie('token', token).json({
-//         id: userDoc._id,
-//         username,
-//         profilePicture: userDoc.profilePicture // agrega la propiedad profilePicture a la respuesta
-//       });
-//     });
-//   } else {
-//     res.status(400).json('wrong credentials');
-//   }
-// });
-
-
-
-
-
-// app.get('/profile', (req, res) => {
-
-//   const { token } = req.cookies;
-//   if (!token) {
-//     return res.status(401).json({ message: 'No se proporcionó un token' });
-//   }
-//   jwt.verify(token, secret, {}, (err, info) => {
-//     if (err) {
-//       return res.status(401).json({ message: 'Token inválido' });
-//     }
-//     res.json(info);
-//     // console.log(info);
-//   });
-// });
-
-
-
-
-// app.post('/logout', (req, res) => {
-//   res.cookie('token', '').json('ok');
-// });
-
-
-// app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-
-//   const { originalname, path } = req.file;
-//   const parts = originalname.split('.');
-//   const ext = parts[parts.length - 1];
-//   const newPath = path + '.' + ext;
-//   fs.renameSync(path, newPath);
-
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, async (err, info) => {
-//     if (err) throw err;
-//     const { title, summary, content, profileAvatar } = req.body;
-//     const postDoc = await Post.create({
-//       title,
-//       summary,
-//       content,
-//       cover: newPath,
-//       profilePicture: profileAvatar,
-//       author: info.id,
-//     });
-//     res.json(postDoc);
-//   });
-
-// });
-
-
-
-
-// app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-//   let newPath = null;
-//   if (req.file) {
-//     const { originalname, path } = req.file;
-//     const parts = originalname.split('.');
-//     const ext = parts[parts.length - 1];
-//     newPath = path + '.' + ext;
-//     fs.renameSync(path, newPath);
-//   }
-
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, async (err, info) => {
-//     if (err) throw err;
-//     const { id, title, summary, content } = req.body;
-//     const postDoc = await Post.findById(id);
-//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-//     if (!isAuthor) {
-//       return res.status(400).json('you are not the author');
-//     }
-//     await postDoc.update({
-//       title,
-//       summary,
-//       content,
-//       cover: newPath ? newPath : postDoc.cover,
-//     });
-
-//     res.json(postDoc);
-//   });
-
-// });
-
-
-
-// app.get('/post', async (req, res) => {
-//   res.json(
-//     await Post.find()
-//       .populate('author', ['username'])
-//       .sort({ createdAt: -1 })
-//       .limit(50)
-//   );
-// });
-
-// app.get('/post/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const postDoc = await Post.findById(id).populate('author', ['username']);
-//   res.json(postDoc);
-// })
-
-// app.listen(port, () => {
-//   console.log('Runnig SERVER ' + port);
-// });
-// //
-
-
-
-const express = require('express');
-const cors = require('cors');
-const mongoose = require("mongoose");
-const User = require('./models/User');
-const Post = require('./models/Post');
-const Suscriptor = require('./models/Suscribe');
-const bcrypt = require('bcryptjs');
-const app = express();
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const multer = require('multer');
-const nodemailer = require('nodemailer');
-const path = require('path');
-
-
-const uploadMiddleware = multer({
-  dest: 'uploads/',
-  limits: {
-    fileSize: 10 * 1024 * 1024 // 10 megabytes
-  }
-});
-
-const fs = require('fs');
-const dotenv = require('dotenv').config();
-
-const port = process.env.PORT || 4000;
-const uri = process.env.REACT_APP_URI
-
-const bodyParser = require('body-parser');
-
-// Usar body-parser para procesar los datos en el cuerpo de las solicitudes entrantes
-const jsonParser = bodyParser.json({ limit: '50mb' });
-const urlencodedParser = bodyParser.urlencoded({ limit: '50mb', extended: true });
-
-// utilizar los analizadores de cuerpo en la aplicación
-app.use(jsonParser);
-app.use(urlencodedParser);
-
-const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-
-// app.use(cors({credentials:true,origin:'https://sentidos.vercel.app/'}));
-// sin paquete cors
-const cors = require('cors');
-
-const corsOptions = {
-  origin: '*',
-};
-
-app.use(cors(corsOptions));
-
-
-// index.js
-
-// module.exports = (req, res) => {
-//   const message = "Hola, mundo estamos aqui por decir que si";
-//   const json = { message };
-
-//   res.status(200).json(json);
-// };
-
-
-
-// con el  paquete cors
-
-// app.use(cors({
-//   origin: 'https://sentidos.vercel.app/',
-//   methods: ['POST', 'PUT', 'GET'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));
-
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,    // usa el nuevo parser de URL
-  useUnifiedTopology: true, // utiliza la nueva topología unificada
-
-}).then(() => {
-  console.log('Conexión exitosa a la base Mongo database');
-}).catch((error) => {
-  console.log('Error al conectar a la base de datos:', error);
-});
 
 
 app.post('/register', uploadMiddleware.single('profilePicture'), async (req, res) => {
@@ -523,6 +300,7 @@ app.post('/suscriptores', async (req, res) => {
 
 
 
+
 app.post('/login', async (req, res) => {
 
   const { username, password } = req.body;
@@ -570,7 +348,35 @@ app.post('/logout', (req, res) => {
 });
 
 
-app.post('/api/post', uploadMiddleware.single('file'), async (req, res) => {
+// app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+
+//   const { originalname, path } = req.file;
+//   const parts = originalname.split('.');
+//   const ext = parts[parts.length - 1];
+//   const newPath = path + '.' + ext;
+//   fs.renameSync(path, newPath);
+
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { title, summary, content, profileAvatar } = req.body;
+//     const postDoc = await Post.create({
+//       title,
+//       summary,
+//       content,
+//       cover: newPath,
+//       profilePicture: profileAvatar,
+//       author: info.id,
+//     });
+//     res.json(postDoc);
+//   });
+
+// });
+
+
+
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
   const { originalname, path } = req.file;
   const parts = originalname.split('.');
   const ext = parts[parts.length - 1];
@@ -618,16 +424,9 @@ app.post('/api/post', uploadMiddleware.single('file'), async (req, res) => {
         }
       });
     }
-
-
-
     res.json(postDoc);
   });
 });
-
-
-
-
 
 app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
   let newPath = null;
@@ -677,10 +476,27 @@ app.get('/post/:id', async (req, res) => {
   res.json(postDoc);
 })
 
-
-
-
 app.listen(port, () => {
   console.log('Runnig SERVER ' + port);
 });
 //
+
+
+
+
+// app.use(cors({credentials:true,origin:'https://sentidos.vercel.app/'}));
+// sin paquete cors
+const cors = require('cors');
+
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
+
+
+
+
+
+
+
