008c3403209fdd0453e09d2bc1fdb6a3fad96582
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-31 16:36:37 -0300

    add the other login in the backend

--- index.js
+++ index.js
@@ -175,83 +175,6 @@ app.post('/suscriptors', async (req, res) => {
 
 
 
-
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  
-  if (!userDoc) {
-    return res.status(400).json('User not found');
-  }
-
-  const passOk = bcrypt.compareSync(password, userDoc.password);
-  if (passOk) {
-    // Generate JWT token
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      
-      // Set token as a cookie
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-      }).json({
-        id: userDoc._id,
-        username,
-        profilePicture: userDoc.profilePicture
-      });
-    });
-  } else {
-    res.status(400).json('Wrong credentials');
-  }
-});
-
-
-
-
-app.get('/profile', (req, res) => {
-  const { token } = req.cookies;
-  if (!token) {
-    return res.status(401).json({ message: 'No se proporcionó un token' });
-  }
-
-  jwt.verify(token, secret, {}, (err, info) => {
-    if (err) {
-      return res.status(401).json({ message: 'Token inválido' });
-    }
-
-    // Establecer la cookie en la respuesta
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none'
-    });
-
-    // Devolver el perfil del usuario
-    res.json(info);
-
-    // Mostrar alerta de token válido
-    console.log('Token válido');
-  });
-});
-
-
-
-
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json('ok');
-});
-
-
-app.get('/post', async (req, res) => {
-  res.json(
-    await Post.find()
-      .populate('author', ['username'])
-      .sort({ createdAt: -1 })
-      .limit(50)
-  );
-});
-
 app.get('/post/:id', async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
@@ -317,46 +240,82 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 });
 
 
-app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-  try {
-    let newPath = null;
-    if (req.file) {
-      const { originalname, path } = req.file;
-      const parts = originalname.split('.');
-      const ext = parts[parts.length - 1];
-      newPath = path + '.' + ext;
-      fs.renameSync(path, newPath);
-      console.log(req.headers);
-    }
-
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
       if (err) throw err;
-
-      const { id } = req.params;
-      const { title, summary, content } = req.body;
-
-      const postDoc = await Post.findById(id);
-      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-      if (!isAuthor) {
-        return res.status(400).json('You are not the author');
-      }
-
-      await postDoc.update({
-        title,
-        summary,
-        content,
-        cover: newPath ? newPath : postDoc.cover,
+      res.cookie('token', token).json({
+        id:userDoc._id,
+        username,
       });
-
-      res.json(postDoc);
     });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json('An error occurred while updating the post');
+  } else {
+    res.status(400).json('wrong credentials');
   }
 });
 
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
+
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
+});
+
+app.get('/post', async (req,res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
+  );
+});
+
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
+})
 
 app.listen(port, () => {
   console.log('Runnig SERVER ' + port);
