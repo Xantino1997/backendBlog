e2e6ecd51106ea6a04c1673bc116b601b807a851
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-06-05 09:30:09 -0300

    add the other info

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(jsonParser);
 app.use(urlencodedParser);
 
 app.use(cors({
-  origin: "https://blog-sentidos-2023.vercel.app",
+  origin: "https://sentidos.vercel.app",
   credentials: true
 }));
 
