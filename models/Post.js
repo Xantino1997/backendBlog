1fe949ab822edacbebe55d21f7e43c616f662597
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-26 23:53:27 -0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -6,12 +6,14 @@ const PostSchema = new Schema({
   summary:String,
   content:String,
   cover:String,
+  category:String,
   profilePicture: { type: String },
   author:{type:Schema.Types.ObjectId, ref:'User'},
 }, {
   timestamps: true,
 });
-
+  
 const PostModel = model('Post', PostSchema);
 
 module.exports = PostModel;
+
